bae84e419605b3330b8fffd5e0c0a2a65c5cefb9
yacout96 <36423481+yacout96@users.noreply.github.com>
2018-12-03 23:53:35 +0200

    ghjkld

--- elmawa2f/src/app/app.component.ts
+++ elmawa2f/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title: string = 'My first AGM project';
+  title: string = 'ElMaw2af';
   lat: number = 51.678418;
   lng: number = 7.809007;
 }
